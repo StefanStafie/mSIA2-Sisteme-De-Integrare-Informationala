ec8db3ed6f9a8218a243b1b15a52b06daf734d31
Stafie Stefan <stefanstafie99@gmail.com>
2025-05-11 14:46:18 +0300

    Commit cu lucrurile pe care le aveam deja

---
+++ CLEVEL.sql
@@ -0,0 +1,6 @@
+insert into WINERY.CLEVEL (LEVELID, LEVELNAME)
+values  (1, 'Level'),
+        (2, 'Tanımsız'),
+        (3, 'Senior'),
+        (4, 'Mid'),
+        (5, 'Junior');
---
+++ WAYOFWORKING.sql
@@ -0,0 +1,5 @@
+insert into WINERY.WAYOFWORKING (WAYID, WAYTYPE)
+values  (1, 'Hybrid'),
+        (2, 'Way_of_working'),
+        (3, 'Yerinde / Ofiste'),
+        (4, 'Remote');
